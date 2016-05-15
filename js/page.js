f94cadc417ec1767e8ac830cf4ea49fe780c6dc4
Junnie-Jobs <dj0999@naver.com>
2016-05-15 19:38:31 +0900

    2nd ver

--- js/page.js
+++ js/page.js
@@ -5,26 +5,84 @@ var TODO = (function (window){
 	 var list_html = "<div class='list_wrapper'>" +
             "<div class='list_content z-depth-1'>" +
                 "<div class='list_header'>"+
-                 "<textarea class='list_header_name'>"+
-                 " new Todo " +
-                 "</textarea>"+
+                 "<textarea class='list_header_name'>{{input-value}}</textarea>"+
                "</div>" +
                 "<div class='list_cards'></div>" +
-              "<a class='add_card' href='#''>" + 
-              "Add a Card..." + "</a>" +
+				"<div class='card_composer'>" +
+                    "<div class='add_card_form'>" +
+                      "<textarea class='list_card_composer_textarea'></textarea>" +
+                       "<a class='waves-effect  waves-light btn card_save blue-grey lighten-5'>save</a>" +
+                       "<a class='waves-effect waves-light btn card_cancel blue-grey lighten-5'>cancel</a>" +
+                    "</div>" +
+                    "<a class='add_card' href='#''>Add a Card...</a>" +
+                "</div>" +
             "</div>" +
           "</div>"
 
+      var card_html = "<div class='list_card'>" +
+      						"<div class='list_card_detail'>" +
+                      	 		"<a class='list_card_title modal-trigger' dir='auto' href='#modal1'>{{value}}</a>" +
+                     	 	"</div>" +
+                      "</div>"
+
 	function init(){
 
-		$(".add_list").on("click", create_list);
+		$(".btn-floating").on("click", create_list);
+		$(".save").on("click", add_list);
+		$(".add_card").on("click", add_card);
+		$(".card_save").on("click", card_save);
+		$(".list_cards").on("click", "a", modal);
+		$( "#sortable" ).sortable();
+		$(".add_list a.cancel").on("click", cancel);
+		
+	}
+
+	function cancel(){
+
+		$(".btn-floating").css('display','block');
+		$(".add_list_form").css('display','none');
+
+	}
+
+	function modal(){
+		$('.modal-trigger').leanModal();
 	}
 
 	function create_list(){
 
-		$(".add_list").before(list_html);
+		$(".btn-floating").css('display','none');
+		$(".add_list_form").css('display','block');
+	}
+
+	function add_card(e){
+
+		console.log("asd");
+		$(e.target).parent(".card_composer").find(".add_card_form").css('display', 'block');
+		$(e.target).parent(".card_composer").find("a.add_card").css('display', 'none');
+	}
+	function card_save(e){
+
+		$(".add_card_form").css('display', 'none');
+		var card_text = $(e.target).parent(".add_card_form").find(".list_card_composer_textarea").val();
+		var $list_wrapper = $(e.target).closest(".list_wrapper");
+		var str = card_html.replace(/\{\{value\}\}/gi,card_text);
+		$list_wrapper.find(".list_cards").append(str);
+		$(e.target).parent(".add_card_form").find(".list_card_composer_textarea").val("");
+		$(e.target).parents(".card_composer").find("a.add_card").css('display', 'block');
+
 	}
 	
+	function add_list(){
+
+		var list_name = $("#add_list").val();
+		var str = list_html.replace(/\{\{input-value\}\}/gi,list_name);
+		$(".add_list").before(str);
+		$("#add_list").val("");
+		$(".add_list_form").css('display','none');
+		$(".btn-floating").css('display','block');
+
+
+	}
 
 	return {
 		"init" : init
